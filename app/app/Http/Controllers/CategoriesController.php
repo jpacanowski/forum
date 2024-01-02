28e2e1a9f376bd9942ab9f72a51dabafa767b09a
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2024-01-02 19:52:30 +0100

    Categories -> input validation

--- app/app/Http/Controllers/CategoriesController.php
+++ app/app/Http/Controllers/CategoriesController.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Settings;
+use App\Http\Requests\CategoryRequest;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
@@ -38,26 +39,15 @@ class CategoriesController extends Controller
     }
 
     // Store category
-    public function store(Request $request, Category $category) {
-        $formFields = $request->validate([
-            'name' => 'required|min:3',
-            'slug' => 'required|min:3'
-        ]);
-
-        $category = Category::create($formFields);
-
+    public function store(CategoryRequest $request, Category $category) {
+        $category = Category::create($request->validated());
         return redirect()->route('categories.edit', $category)
             ->with('info', 'Category has been created successfully');
     }
 
     // Update category data
-    public function update(Request $request, Category $category) {
-        $formFields = $request->validate([
-            'name' => 'required|min:3',
-            'slug' => 'required|min:3'
-        ]);
-
-        $category->update($formFields);
+    public function update(CategoryRequest $request, Category $category) {
+        $category->update($request->validated());
         return back()->with('info', 'Category has been updated successfully');
     }
 
---
+++ app/app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return match($this->method()) {
+            'POST' => $this->store(),
+            'PUT' => $this->update(),
+            'DELETE' => $this->destroy()
+        };
+    }
+
+    protected function store(): array
+    {
+        return [
+            'name' => 'required|min:3',
+            'slug' => 'required|min:3'
+        ];
+    }
+
+    protected function update(): array
+    {
+        return [
+            'name' => 'required|min:3',
+            'slug' => 'required|min:3'
+        ];
+    }
+
+    protected function destroy(): array
+    {
+        return [
+            'id' => 'required|integer|exists:categories,id'
+        ];
+    }
+}
